effaa21752304aa8c64e072c5fef9bef066ea0ba
Fluteras <mat.fluteras@hotmail.fr>
2023-11-15 15:28:53 +0100

    Exercice Création d'une Gateway

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\VideoGameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,9 +30,103 @@ class ApiController extends AbstractController
         $games = $videoGameRepository->findAll();
         $array = array();
         foreach ($games as $game) {
-            array_push($array, ["id" => $game->getId()], ["name"=> $game->getName()]);
+            array_push($array, ["id" => $game->getId(), "name" => $game->getName()]);
         }
 
         return $this->json($array);
+    }
+
+    #[Route('/api/convert', name: 'app_api_convert')]
+    public function convertCsvToJson(): Response
+    {
+        $gamesInfo = array();
+        if (($handle = fopen("D:/Programmes/Logiciels/laragon/www/ilovegamer/Ilovegamer/var/data/games.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                array_push($gamesInfo, $data);
+            }
+            fclose($handle);
+        }
+
+        return $this->json($gamesInfo);
+    }
+
+    #[Route('/api/gateway', name: 'app_api_gateway')]
+    public function gateway(VideoGameRepository $videoGameRepository): Response
+    {
+        $videoGames = $videoGameRepository->findAll();
+        $games = array();
+        foreach ($videoGames as $game) {
+            array_push($games, ["id" => $game->getId(),"name" => $game->getName()]);
+        }
+        $gamesInfo = array();
+        if (($handle = fopen("D:/Programmes/Logiciels/laragon/www/ilovegamer/Ilovegamer/var/data/games.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                array_push($gamesInfo, $data);
+            }
+            fclose($handle);
+        }
+
+        $gameDatas = array();
+        $indexsInDB = array();
+
+        $gameInfoNameIndex = 0;
+        $firstLine = true;
+        foreach ($gamesInfo as $gameInfo) {
+            
+            if($firstLine)
+            {
+                for ($i=0; $i < count($gameInfo) ; $i++) {                
+                    if($gameInfo[$i] == "Game Title")
+                    {
+                        $gameInfoNameIndex = $i;
+                        break;
+                    }
+                }
+                $firstLine = false;
+            }
+            else
+            {
+                $unique = true;
+                foreach ($games as $game) {
+                    if($game["name"] == $gameInfo[$gameInfoNameIndex])
+                    {               
+                        $unique = false;
+                        unset($gameInfo[$gameInfoNameIndex]);
+                        $callback = function() use ($gamesInfo, $gameInfo) : array
+                        {
+                            $values = array();
+                            $key = 0;
+                            foreach ($gamesInfo[0] as $gameInfoKey) {
+                                if($gameInfoKey != "Game Title")
+                                {
+                                    array_push($values, [$gameInfoKey => $gameInfo[$key]]);
+                                }
+                                $key++;
+                            }
+                            return $values;
+                        };
+                        array_push($gameDatas, ["id" => $game["id"], "name"=> $game["name"],$callback()]);
+                        
+                        array_push($indexsInDB, $game["id"]);
+                        break;
+                    }
+                }
+
+                if($unique)
+                {
+                    array_push($gameDatas, $gameInfo);
+                }
+                
+                
+            }            
+        }
+        foreach ($games as $game) {
+            if(!in_array($game["id"],$indexsInDB))
+            {
+                array_push($gameDatas, $game);
+            }
+        }
+
+        return $this->json($gameDatas);
     }
 }
